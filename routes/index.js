feb35e6f45f297197806c52b66dbd10282accc8d
martinbarker <martinbarker99@gmail.com>
2021-02-01 18:37:55 -0800

    unix

--- routes/index.js
+++ routes/index.js
@@ -462,6 +462,77 @@ app.get(/^\/tagger\/(.*)/, async function (req, res) {
   });
 })
 
+//any route that starts with unix
+app.get('/unix', async function (req, res) {
+  unixRoute(req, res)
+})
+
+//any route that starts with unix
+app.get(/^\/unix\/(.*)/, async function (req, res) {
+  unixRoute(req, res)
+})
+
+async function unixRoute(req, res){
+  let unxiTimestamp = req.params[0];
+  console.log('url unxiTimestamp=', unxiTimestamp)
+
+  //get mainTemplate data
+  let mainTemplateData = await getMainTemplateData(req.params.id)
+  let displayPosts = mainTemplateData.postsDisplay;
+
+  res.render('unix', {
+    //template layout to use
+    layout: 'mainTemplate',
+    //page title of tab
+    pageTitle: 'Unix Converter',
+    //discogs url
+    unxiTimestamp: unxiTimestamp,
+    //page tab icon
+    icon: 'https://cdn.iconscout.com/icon/free/png-512/unix-1-599990.png',
+    //shareable preview-cart metadata
+    previewCardTitle:'Convert Unix Timestamp',
+    previewCardUrl:'http://www.martinbarker.me/unix',
+    previewCardWebsite:'website',
+    previewCardDescription:'Convert a Unix Timestamp to a Readable DateTime',
+    previewCardImage:'https://i.imgur.com/f0xepPT.jpg',
+    //expand projects tab
+    projects: 'active',
+    //set active current tab
+    unix: 'active',
+    //body content title 
+    pageBodyNavTitle: 'Unix Timestamp Converter',
+    //body content github link
+    pageBodyNavGithub: 'https://github.com/MartinBarker/martinbarker/blob/master/views/unix.handlebars',
+    //list to display for navbar 'Blog' options
+    posts: displayPosts,
+    //mainTemplateData
+    imgPath: '/' + mainTemplateData.imgPath,
+    imgSrcUrl: mainTemplateData.imgSrc,
+    imgListen: mainTemplateData.imgListen,
+    textColor1: mainTemplateData.colorData.textColor1, //'Martin Barker' Navbar Header text color
+    backgroundColor1: mainTemplateData.colorData.backgroundColor1, //'Martin Barker' Navbar Header Background Color
+    textColor6: mainTemplateData.colorData.textColor6, //sidebar un-active tab text color
+    backgroundColor2: mainTemplateData.colorData.backgroundColor2, //sidebar un-active tab background color
+    textColor2: mainTemplateData.colorData.textColor2, //sidebar active tab
+    backgroundColor3: mainTemplateData.colorData.backgroundColor3, //sidebar active tab
+    textColor7: mainTemplateData.colorData.textColor7, //sidebar lower background
+    backgroundColor7: mainTemplateData.colorData.backgroundColor7, //sidebar lower background
+    textColor3: mainTemplateData.colorData.textColor3, //sidebar hover tab color
+    backgroundColor4: mainTemplateData.colorData.backgroundColor4, //sidebar hover tab color
+    textColor4: mainTemplateData.colorData.textColor4, //body header title color
+    backgroundColor6: mainTemplateData.colorData.backgroundColor6, //body header title color
+    textColor5: mainTemplateData.colorData.textColor5, //body color
+    backgroundColor5: mainTemplateData.colorData.backgroundColor5, //body color
+    //img color display boxes
+    Vibrant: mainTemplateData.colorData.Vibrant,
+    LightVibrant: mainTemplateData.colorData.LightVibrant,
+    DarkVibrant: mainTemplateData.colorData.DarkVibrant,
+    Muted: mainTemplateData.colorData.Muted,
+    LightMuted: mainTemplateData.colorData.LightMuted,
+    DarkMuted: mainTemplateData.colorData.DarkMuted,
+  });
+}
+
 //api route to return pageColors
 app.post('/getColors', async function (req, res) {
   //let filepath = req.body.filepath
